a55694335625f518e34c854227c3f6a8961a30b7
hasnatulhaq <hasnatulhaq@outlook.com>
2022-02-18 17:29:29 +0500

    feat(message):updating.....

--- linkedin/src/Components/messagepanel/messagepanel.js
+++ linkedin/src/Components/messagepanel/messagepanel.js
@@ -1,5 +1,6 @@
 import Topheading from "../heading/heading";
 import Threedots from "../threedot/threedots";
+import Writemessage from "../writemessage/writemessage"
 
 function Chatpanel(){
     return(
@@ -11,6 +12,7 @@ function Chatpanel(){
             <p>Abdul basit kazmi</p>
             <p>Tag line</p>
         </div>
+        <Writemessage/>
         </>
     )
 }
---
+++ linkedin/src/Components/writemessage/writemessage.js
@@ -0,0 +1,14 @@
+import './writemessage.css'
+function writemessage(){
+    return (
+          <>
+          <div className="chatbox__writemessage">
+              <h1>hello world</h1>
+          <input type='text'></input>
+          </div>
+          </>
+    )
+}
+
+
+export default writemessage;
