b4bca9f03e981ad4a4d0798f57074629805ebb2b
hasnatulhaq <hasnatulhaq@outlook.com>
2022-03-04 17:29:06 +0500

    feat(updating)...

--- linkedin/src/Components/profileview/profileview.js
+++ linkedin/src/Components/profileview/profileview.js
@@ -1,6 +1,28 @@
-function ProfileView(){
+function Redbox(){
+  return(
+    <>
+    <Greenbox/>
+       <h1>hello world</h1>
+    </>
+  )
+}
+
+function Greenbox(){
+  return(
+    <>
+     <Redbox/>
+       <h1>hello world</h1>
+       <ProfileView/>
+    </>
+  )
+}
+
+
+
+export default function ProfileView(){
     return(
         <>
+        <Redbox/>
         <div className="main-wrapper">
            <div>
              </div> 
@@ -11,3 +33,6 @@ function ProfileView(){
         </>
     )
 }
+
+
+
